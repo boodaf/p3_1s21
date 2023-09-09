9028a2b0f54ddd68e6d3b2b741eec79a41688084
boodaf <144178847+boodaf@users.noreply.github.com>
2023-09-08 20:39:26 -0600

    Add files via upload

---
+++ p3_1s21.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+class p3_1s21{
+   public static void main(String[] args) {
+      Scanner teclado = new Scanner(System.in);
+      String cadena;
+      System.out.printl18 añosn("Ingresa tu nombre ");
+      cadena = teclado.nextLine();
+      System.out.println("Bienvenido"+ cadena + ", a la clase"); 
+      cadena = teclado.nextLine();
+      System.out.println("Ingresa tu nombre");
+      cadena = teclado.nextLine();
+      System.out.println("Bienvenido " + cadena +",a la clase");
+      cadena = teclado.nextLine();
+      System.out.println("Ingresa tu edad");
+      cadena = teclado.nextLine();
+      System.out.println("Ingresa tu correo");
+      cadena = teclado.nextLine();
+      System.out.println("Ingresa tu numero celular");
+
+
+
+    
+{}
+
+
